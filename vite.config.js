2bfacef8f2fc931983e1f66c6cab20366b190d73
Shiven Sharma <shiven676@gmail.com>
2025-12-25 23:45:21 +0530

    Updated Tailwind config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,18 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path';
 import autoprefixer from 'autoprefixer';
 import { fileURLToPath } from 'url';
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), autoprefixer()],
+  plugins: [react(), tailwindcss()], 
+  css: {
+    postcss: {
+      plugins: [autoprefixer()],
+    },
+  },
+  
   define: {
     'process.env': {}
   },
